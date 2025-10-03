2ffd2ad60d119878ec74493b2d4209e658a362b6
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2025-10-03 07:05:34 +0200

    added docs in broadcast header

--- src/broadcast.h
+++ src/broadcast.h
@@ -10,8 +10,17 @@ struct broadcast_header_st {
 
 typedef struct broadcast_header_st broadcast_header_t;
 
+/**
+ * @brief Broadcast receive callback function
+ * This is the callback function for the broadcast protocol. It is used to receive the broadcast packets.
+ */
 typedef void (*breadcast_recv_cb_fn)(uint8_t *data, uint16_t size, uint32_t from, int16_t rssi);
 
+/**
+ * @brief Broadcast protocol packet 
+ * This is the packet class for the broadcast protocol. 
+ * Allocate the memory for the broadcast header and the payload.
+ */
 class BroadCastPacket: public RadioPacket {
 public:
 	explicit BroadCastPacket(pktbufSentCbFn cb, void *arg): RadioPacket(cb, arg) { setIsBroadcast(); }
@@ -21,7 +30,11 @@ public:
 	uint8_t *broadcastPayload() { return (uint8_t *)clearData()+sizeof(broadcast_header_st); }
 };
 
-
+/**
+ * @brief Broadcast protocol implementation
+ * This is the most basic broadcast protocol implementation. It is used to send and receive broadcast 802.11 packets.
+ * This implementation don't provide a port so there can be only one receive callback.
+ */
 class Broadcast: public PacketBufProtocol {
 public:
 	Broadcast(PacketBuf *pbuf);
